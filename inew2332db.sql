d571556f46555857fa10a9bde589f766c28448d9
Lam Nguyen <me@madebylam.com>
2013-10-07 20:38:21 -0500

    replaced database.sql to working version

---
+++ inew2332db.sql
@@ -0,0 +1,257 @@
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: inew2332
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.13.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `devices`
+--
+
+DROP TABLE IF EXISTS `devices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `devices` (
+  `device_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `mac_address` varchar(50) NOT NULL,
+  `device_name` varchar(50) NOT NULL,
+  `device_status` int(11) NOT NULL,
+  `register_date` date DEFAULT NULL,
+  `activate_date` date DEFAULT NULL,
+  `deactivate_date` date DEFAULT NULL,
+  `deregister_date` date DEFAULT NULL,
+  `device_type` int(11) NOT NULL,
+  `device_type_specify` varchar(25) DEFAULT NULL,
+  `os_system` int(11) DEFAULT NULL,
+  `os_version` varchar(25) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `modify_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`device_id`),
+  UNIQUE KEY `mac_address` (`mac_address`),
+  KEY `user_indx` (`user_id`),
+  KEY `dev_stat_indx` (`device_status`),
+  KEY `dev_typ_indx` (`device_type`),
+  KEY `os_sys_indx` (`os_system`),
+  CONSTRAINT `devices_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  CONSTRAINT `devices_ibfk_2` FOREIGN KEY (`device_status`) REFERENCES `ref_device_status` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `devices_ibfk_3` FOREIGN KEY (`device_type`) REFERENCES `ref_device_type` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `devices_ibfk_4` FOREIGN KEY (`os_system`) REFERENCES `ref_os_system` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `devices`
+--
+
+LOCK TABLES `devices` WRITE;
+/*!40000 ALTER TABLE `devices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `devices` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_bi_devices_ts
+BEFORE INSERT ON devices
+FOR EACH ROW
+BEGIN
+   SET new.create_date = CURRENT_TIMESTAMP();
+   SET new.modify_date = CURRENT_TIMESTAMP();
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_bu_devices_ts
+BEFORE UPDATE ON devices
+FOR EACH ROW
+BEGIN
+   SET new.modify_date = CURRENT_TIMESTAMP();
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `ref_device_status`
+--
+
+DROP TABLE IF EXISTS `ref_device_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ref_device_status` (
+  `id` int(11) NOT NULL,
+  `device_status_description` varchar(50) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ref_device_status`
+--
+
+LOCK TABLES `ref_device_status` WRITE;
+/*!40000 ALTER TABLE `ref_device_status` DISABLE KEYS */;
+INSERT INTO `ref_device_status` VALUES (1,'Active','2013-10-08 01:29:34'),(2,'Inactive','2013-10-08 01:29:34'),(3,'Deregister','2013-10-08 01:29:34'),(4,'Pending','2013-10-08 01:29:34'),(5,'Revoked','2013-10-08 01:29:34');
+/*!40000 ALTER TABLE `ref_device_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ref_device_type`
+--
+
+DROP TABLE IF EXISTS `ref_device_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ref_device_type` (
+  `id` int(11) NOT NULL,
+  `device_type_description` varchar(50) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ref_device_type`
+--
+
+LOCK TABLES `ref_device_type` WRITE;
+/*!40000 ALTER TABLE `ref_device_type` DISABLE KEYS */;
+INSERT INTO `ref_device_type` VALUES (1,'Phone','2013-10-08 01:29:34'),(2,'Laptop','2013-10-08 01:29:34'),(3,'Tablet','2013-10-08 01:29:34'),(4,'Blackberry','2013-10-08 01:29:34'),(5,'Other','2013-10-08 01:29:34');
+/*!40000 ALTER TABLE `ref_device_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ref_os_system`
+--
+
+DROP TABLE IF EXISTS `ref_os_system`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ref_os_system` (
+  `id` int(11) NOT NULL,
+  `os_system_description` varchar(50) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ref_os_system`
+--
+
+LOCK TABLES `ref_os_system` WRITE;
+/*!40000 ALTER TABLE `ref_os_system` DISABLE KEYS */;
+INSERT INTO `ref_os_system` VALUES (1,'Android','2013-10-08 01:29:34'),(2,'IOS','2013-10-08 01:29:34'),(3,'Windows','2013-10-08 01:29:34'),(4,'Linux','2013-10-08 01:33:58');
+/*!40000 ALTER TABLE `ref_os_system` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_id` varchar(25) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(35) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `modify_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_bi_users_ts
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+   SET new.create_date = CURRENT_TIMESTAMP();
+   SET new.modify_date = CURRENT_TIMESTAMP();
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_bu_users_ts
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+   SET new.modify_date = CURRENT_TIMESTAMP();
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-10-07 20:37:07
