f5a01c6514c9ac8a9d6310ad651213d6eb49ddbf
rduncan19 <riseofredhorn@gmail.com>
2013-11-18 19:53:33 -0600

    FAQ page

---
+++ site/faq.php
@@ -0,0 +1,56 @@
+<?php include "header.php" ?>
+
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>FAQ</h1>
+          <p class=MsoNormal>FAQ's Page</p>
+
+<p class=MsoNormal>1. <span class=GramE>how</span> do I find my mac address in
+windows?</p>
+
+<p class=MsoNormal><span style='mso-spacerun:yes'> </span>- While connected to
+the internet, <span class=GramE>Under</span> network and sharing center, click
+the link Local Area Connection to open another window, and then click on the
+Details button. Look for the Physical <span class=GramE>Address,</span> this is
+your Mac Address. </p>
+
+<p class=MsoNormal>- Or, open a Command prompt window and type &quot;<span
+class=SpellE>ipconfig</span> /all&quot; in the command line. Look for physical
+address or MAC Address.</p>
+
+<p class=MsoNormal>2. <span class=GramE>Cant</span> log into my account. </p>
+
+<p class=MsoNormal>- Make sure your user name and <span class=GramE>password
+are</span> correct. Make sure the caps locks are not on.</p>
+
+<p class=MsoNormal>- try using the reset password button and resend a temporary
+password to your email.</p>
+
+<p class=MsoNormal>3. <span class=GramE>how</span> do I delete a device from my
+account?</p>
+
+<p class=MsoNormal>- consult the IT Security dept.</p>
+
+<p class=MsoNormal>4. How do I update my personal or account information?</p>
+
+<p class=MsoNormal>- After you log onto your personal account, you can edit
+your information by clicking the edit button next to the section you wish
+update. When you are done, simply click &quot;Update&quot;. And you are done.</p>
+
+<p class=MsoNormal>5. Can anyone else see my account?</p>
+
+<p class=MsoNormal>- No one else can see your personal account except you and
+the IT Security Dept.</p>
+
+<p class=MsoNormal>6. How many Devices can I add under my account?</p>
+
+<p class=MsoNormal>- You can add up to 5 devices.</p>
+
+<p class=MsoNormal>7. Can I delete my own <span class=GramE>account.</span> </p>
+
+<p class=MsoNormal>- No you cannot personally delete your own account. That is
+an IT Security Dept job. </p>
+        </div>
+      </div>
+
+<?php include "footer.php" ?>
