8cc7d11afb726c4c27f327ac815f57d2954b9f79
rduncan19 <riseofredhorn@gmail.com>
2013-11-18 19:22:13 -0600

    Policy.php

---
+++ site/policy.php
@@ -0,0 +1,314 @@
+<?php include "header.php" ?>
+
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Employee Fair and Responsible User Policies at Circle Square</h1>
+		  <h2>Purpose:</h2>
+		     <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        Circle Square provides various electronic services to our Employees and business associates or allows the use of certain technology as a productivity tool to assist in the performance of business-related activities.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        This guideline governs the use of these electronic services, equipment, and technology administered, facilitated or made accessible by Circle Square:
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <ul class="c5 lst-kix_f8h6rlb9wm94-0 start">
+      <li class="c0 c6">
+        <span class="c4">
+          Circle Square&#39;s information technology, such as any administered, owned, or leased technology, equipment, computer, or network systems;
+        </span>
+      </li>
+    </ul>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <ul class="c5 lst-kix_izeuii9vx1zh-0 start">
+      <li class="c6 c0">
+        <span class="c4">
+          all electronic services authorized and supported by Circle Square, all computers attached to Circle Square&rsquo;s networks, and stand-alone computers with remote access to Circle Square&rsquo;s networks.
+        </span>
+      </li>
+    </ul>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        This guideline is designed to protect Circle Square from error, fraud, misuse, alteration, theft, copyright violation, and sabotage, and to protect employees from unlawful conduct, including discrimination, harassment, and retaliation.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        This guideline applies to any Employee or other person authorized by Circle Square to use Circle Square IT. All equipment or technology that is owned or provided, facilitated or administered by Circle Square is included within this guideline&rsquo;s scope.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        Any violation of this guideline may lead to disciplinary action, up to and including separation of employment and other appropriate legal action.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c2">
+        Policy:
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c2">
+        Proper Use of Technology
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        Circle Square provides access to its technology systems to assist employees in performing their duties efficiently and effectively. Inappropriate or unauthorized use of information technology exposes Circle Square to internal and external risks and may reduce the effectiveness of those systems. All users of Circle Square IT are responsible for using that technology in an appropriate and lawful manner and in compliance with all Circle Square guidelines.
+      </span>
+    </p>
+    <p class="c1 c0">
+      <span class="c4">
+      </span>
+    </p>
+    <p class="c1 c0">
+      <span class="c4">
+      </span>
+    </p>
+    <p class="c0 c1">
+      <span class="c4">
+      </span>
+    </p>
+    <p class="c1 c0">
+      <span class="c4">
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c2">
+        Privacy and Monitoring/Search
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        All files, e-communications, content, data, and other information stored, posted, or transmitted using Circle Square IT are the property of Circle Square, not the user.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c2">
+        Acceptable Usage
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        Circle Square IT is intended for legitimate Circle Square business only. Users must be careful how they use Circle Square IT, e-communications and Circle Square social media. Inappropriate communications or content may have significant workplace morale issues or legal consequences, such as claims of discrimination, antitrust violation, theft of trade secret, or false advertising. Any software or hardware found on Circle Square IT that has not been approved by IS may be removed, along with associated files, without notice to the employee.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        Unacceptable Use: The following are examples of unacceptable or inappropriate use of Circle Square IT and official Circle Square social media sites and are prohibited:
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <ul class="c5 lst-kix_53gs2fs3x8cl-0 start">
+      <li class="c6 c0">
+        <span class="c4">
+          Data which the employee is not authorized to access or logging into a server or user account that the employee is not expressly authorized to access.
+        </span>
+      </li>
+    </ul>
+    <ul class="c5 lst-kix_ecdlts83r151-0 start">
+      <li class="c6 c0">
+        <span class="c4">
+          Adding or removing Circle Square hardware or software, attaching external devices, or making configuration changes to Circle Square IT without approval by IS;
+        </span>
+      </li>
+      <li class="c6 c0">
+        <span class="c4">
+          Using Circle Square IT for personal use, including for e-communications, that interferes with the user&rsquo;s work performance, negatively impacts the operation of Circle Square or its systems.
+        </span>
+      </li>
+    </ul>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        Confidential Information: Carefully consider the confidentiality and sensitivity of any material stored or transmitted using Circle Square IT, e-communications or Circle Square social media. Portable data storage devices such as jump drives or portable hard drives should not be used to store or transport Circle Square confidential information without the express consent of the appropriate manager.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c2">
+        User Accountability
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        If your company is involved in litigation, then your personal smartphone used for work&mdash;even merely for receiving corporate email&mdash;can be seized and searched for evidence during the discovery phase.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        If you&#39;re going to use this device for business, then you must comport to company policies and standards for keeping information confidential.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        We include the language about which platforms we support, whether or not we reimburse for various charges, what state the device must be in (i.e. not rooted or Jailbroken) and what applications can and can&#39;t be used while on the network.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        A personal device can be connected to the infrastructure, but the user is personally liable for the device and any carrier service costs. Users of personal devices are not eligible for expense reimbursement for hardware or carrier services. Users of personal devices must agree to all terms and conditions in this policy to
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        be allowed access to the our services.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        Employees that purchase a device on their own that is not in line with our standard approved device list may not be allowed to have their devices added to our business and technology infrastructure.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        We hereby acknowledge that the use of a personal device in connection with our business carries specific risks for which you, as the user, assume full liability.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        These risks include, but are not limited to, the partial or complete loss of data as a result of a crash of the OS, errors, bugs, viruses, and/or other software or hardware failures, or programming errors which could render a device inoperable.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        We hereby disclaim liability for the loss of any such data and/or for service interruptions. We expressly reserve the right to wipe the entire device, or specifically managed applications, at any time as deemed necessary for purposes of protecting or maintaining our service.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        We reserve the right, at its own discretion, to remove any of our supplied applications from your device as a result of an actual or deemed violation of our Device Policy.
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c3">
+        &nbsp;
+      </span>
+    </p>
+    <p class="c0">
+      <span class="c4">
+        We have the right to monitor you when you log into our app. We have the right take a screenshot of the unauthorized access you do.
+      </span>
+    </p>
+        </div>
+      </div>
+
+<?php include "footer.php" ?>
