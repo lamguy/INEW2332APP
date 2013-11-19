9c247e68ed157b2691009f9f9b0d05e874ea850f
rduncan19 <riseofredhorn@gmail.com>
2013-11-18 19:58:54 -0600

    faq

--- site/faq.php
+++ site/faq.php
@@ -18,7 +18,7 @@ your Mac Address. </p>
 class=SpellE>ipconfig</span> /all&quot; in the command line. Look for physical
 address or MAC Address.</p>
 
-<p class=MsoNormal>2. <span class=GramE>Cant</span> log into my account. </p>
+<p class=MsoNormal><strong>2. <span class=GramE>Cant</span> log into my account. </strong></p>
 
 <p class=MsoNormal>- Make sure your user name and <span class=GramE>password
 are</span> correct. Make sure the caps locks are not on.</p>
@@ -26,27 +26,27 @@ are</span> correct. Make sure the caps locks are not on.</p>
 <p class=MsoNormal>- try using the reset password button and resend a temporary
 password to your email.</p>
 
-<p class=MsoNormal>3. <span class=GramE>how</span> do I delete a device from my
-account?</p>
+<p class=MsoNormal><strong>3. <span class=GramE>how</span> do I delete a device from my
+account?</strong></p>
 
 <p class=MsoNormal>- consult the IT Security dept.</p>
 
-<p class=MsoNormal>4. How do I update my personal or account information?</p>
+<p class=MsoNormal><strong>4. How do I update my personal or account information?</strong></p>
 
 <p class=MsoNormal>- After you log onto your personal account, you can edit
 your information by clicking the edit button next to the section you wish
 update. When you are done, simply click &quot;Update&quot;. And you are done.</p>
 
-<p class=MsoNormal>5. Can anyone else see my account?</p>
+<p class=MsoNormal><strong>5. Can anyone else see my account?</strong></p>
 
 <p class=MsoNormal>- No one else can see your personal account except you and
 the IT Security Dept.</p>
 
-<p class=MsoNormal>6. How many Devices can I add under my account?</p>
+<p class=MsoNormal><strong>6. How many Devices can I add under my account?</strong></p>
 
 <p class=MsoNormal>- You can add up to 5 devices.</p>
 
-<p class=MsoNormal>7. Can I delete my own <span class=GramE>account.</span> </p>
+<p class=MsoNormal><strong>7. Can I delete my own <span class=GramE>account.</strong></span> </p>
 
 <p class=MsoNormal>- No you cannot personally delete your own account. That is
 an IT Security Dept job. </p>
